c8533cb0e06775d955c525c191dbb2c18cffbdfb
bsakari <sakaribenjamin@gmail.com>
2019-02-25 16:53:23 +0300

    first commit

---
+++ src/xyz/fileThree.java
@@ -0,0 +1,20 @@
+package xyz;
+
+public class fileThree {
+    public static void main(String[] args) {
+        String[] wanafunzi;
+        String output;
+        wanafunzi = new String[5];
+        wanafunzi[0] = "Anthony";
+        wanafunzi[1] = "Ian";
+        wanafunzi[2] = "Peter";
+        wanafunzi[3] = "Brio";
+        wanafunzi[4] = "Wasike";
+
+        output = wanafunzi[0]+"\n"+wanafunzi[1]+"\n"+
+                wanafunzi[2]+"\n"+wanafunzi[3]+"\n"+
+                wanafunzi[4];
+
+        System.out.println(output);
+    }
+}
